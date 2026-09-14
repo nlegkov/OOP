0736d745de8cbfdb97adf910070c34ae3ada2e8a
nlegkov <n.legkov@g.nsu.ru>
2026-09-14 09:16:45 +0700

    task1

--- Task_1_1_1/build.sh
+++ Task_1_1_1/build.sh
@@ -4,18 +4,18 @@ set -e
 # Очистка старой сборки
 rm -rf out doc app.jar
 
-echo "=== 1. Компиляция исходного кода (javac) ==="
+echo "1. Компиляция исходного кода (javac)"
 mkdir -p out
 javac -d out src/main/java/ru/nsu/legkov/HeapSort.java
 
-echo "=== 2. Генерация Javadoc ==="
+echo "2. Генерация Javadoc"
 mkdir -p doc
 javadoc -d doc -sourcepath src/main/java -subpackages ru.nsu.legkov -quiet
 
-echo "=== 3. Создание JAR-файла (jar) ==="
+echo "3. Создание JAR-файла (jar)"
 jar --create --file app.jar --main-class ru.nsu.legkov.HeapSort -C out .
 
-echo "=== 4. Запуск приложения (java) ==="
+echo "4. Запуск приложения (java)"
 java -jar app.jar
 
-echo "=== Сборка и запуск завершены успешно! ==="
+echo "Сборка и запуск завершены успешно!"
